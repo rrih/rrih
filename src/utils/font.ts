9ff790ba143aea5f0fa7bc5679e0f33aefc5f5ee
rrih <rsklv@icloud.com>
2022-02-26 22:07:35 +0900

    delete

--- src/utils/font.ts
+++
@@ -1,2 +0,0 @@
-const font: string = '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"'
-export default font
